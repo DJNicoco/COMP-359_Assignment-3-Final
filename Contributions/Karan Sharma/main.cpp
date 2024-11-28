4fd0df8a598c37c74326628e335f5cec02e4c980
karansharmaa <89495471+karansharmaa@users.noreply.github.com>
2024-11-27 16:39:26 -0800

    Update main.cpp

--- Contributions/Karan Sharma/main.cpp
+++ Contributions/Karan Sharma/main.cpp
@@ -2,19 +2,22 @@
 
 int main() {
     // Create a family tree with a root person
-    FamilyTree family("Grandparent");
+    FamilyTree family("Billy The 3rd (Grandpa)");
 
     // Add children to the root
-    family.addChild(family.root, "Parent1");
-    family.addChild(family.root, "Parent2");
+    family.addChild(family.root, "Pablo (child 1)");
+    family.addChild(family.root, "Sofia (child 2)");
 
     // Add grandchildren to Parent1
-    family.addChild(family.root->leftChild, "Child1");
-    family.addChild(family.root->leftChild, "Child2");
+    family.addChild(family.root->leftChild, "Cono");
+    family.addChild(family.root->leftChild, "Jono");
+    family.addChild(family.root->leftChild, "Meowdy");
 
     // Add grandchildren to Parent2
-    family.addChild(family.root->leftChild->rightSibling, "Child3");
-    family.addChild(family.root->leftChild->rightSibling, "Child4");
+    family.addChild(family.root->leftChild->rightSibling, "Pomo");
+    family.addChild(family.root->leftChild->rightSibling, "Poco");
+    family.addChild(family.root->leftChild->rightSibling, "Polo");
+
 
     // Display the family tree
     cout << "Family Tree:\n";
